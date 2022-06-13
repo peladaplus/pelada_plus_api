bdff97de31a9da5227e805ecf12fa836731ee6c3
Lucas Bereta <lucas.bereta@hotmail.com>
2022-06-13 19:07:10 -0300

    :recycle: refactoring class exception

--- src/main/java/com/athenas/pelada_plus_api/exception/GroupExceptionHandler.java
+++ src/main/java/com/athenas/pelada_plus_api/exception/GroupExceptionHandler.java
@@ -8,11 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class GroupExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value= {Exception.class})
-    public ResponseEntity<Object> handleAnyException(Exception e, WebRequest request){
-        return new ResponseEntity<>(e, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleAnyException(Exception e, WebRequest request) {
+        String errorDescription = e.getLocalizedMessage();
+        if (errorDescription == null) errorDescription = e.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorDescription);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 }
