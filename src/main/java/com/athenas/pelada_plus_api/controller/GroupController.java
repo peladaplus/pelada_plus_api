235e5c0d693eaa7e5bfc2be2e9bfa1cc3c8c2173
Lucas Bereta <lucas.bereta@hotmail.com>
2022-06-13 19:08:12 -0300

    :coffin: removing imports not used

--- src/main/java/com/athenas/pelada_plus_api/controller/GroupController.java
+++ src/main/java/com/athenas/pelada_plus_api/controller/GroupController.java
@@ -7,7 +7,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
